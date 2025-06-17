e9f40c13eda6ae10ae88a8caacc5692e6bc18b28
chan000518 <wkdvnddl050701@gmail.com>
2025-06-17 18:13:26 +0900

    chore: 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,13 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from '@/contexts/AuthContext';
 import NavBar from '@/components/Layout/NavBar'
+import { Nanum_Gothic } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const nanumGothic = Nanum_Gothic({
+  subsets: ['latin'],
+  weight: ['400', '700', '800'], // 필요한 굵기만 선택
+  variable: '--font-nanum-gothic', // CSS 변수명으로 Tailwind와 연동 가능
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -27,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nanumGothic.variable} font-nanum antialiased`}
       >
         <AuthProvider>
           <NavBar />
